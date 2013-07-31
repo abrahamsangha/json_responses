3a119c01e05a1ef125f74e77acb2fe279207ac2c
Kevin Solorio <soloriok@gmail.com>
2013-07-31 16:38:06 -0500

    add ajax call to post and use template with json data

--- public/js/application.js
+++ public/js/application.js
@@ -1,2 +1,34 @@
 $(document).ready(function() {
+
+  $('#add-task').on('click', function(event){
+    event.preventDefault();
+    var url = $(this).attr('href');
+
+    $(this).hide();
+    $.get(url, function(task_form){
+      $('.container').append(task_form);
+    });
+  });
+
+  $('.container').on('submit', '#new-task-form', function(event){
+    event.preventDefault();
+    var url = $(this).attr('action');
+    var data = $(this).serialize();
+
+    $.post(url, data, function(task){
+      console.log(task);
+      var li = todoTemplate(task);
+      $('.incomplete ul').append(li);
+
+    }, 'json');
+
+    $('#new-task-form').remove();
+    $('#add-task').show();
+
+  });
+
+  function todoTemplate(task){
+    return "<li>" + task.name + "</li>";
+  };
+
 });
