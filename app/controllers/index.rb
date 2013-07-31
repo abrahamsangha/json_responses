6804bb9821184cfef8db617df51f15b56d8b8bb7
Kevin Solorio <soloriok@gmail.com>
2013-07-31 15:23:55 -0500

    implement controller actions for adding tasks

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,17 @@
 get '/' do
-  # Look in app/views/index.erb
+  @tasks = Task.all
   erb :index
+end
+
+get '/tasks/new' do
+  erb :new, layout: !request.xhr?
+end
+
+post '/tasks' do
+  @task = Task.create(params[:task])
+  if request.xhr?
+    erb :_todo_li, layout: false, locals: {task: @task}
+  else
+    redirect to '/'
+  end
 end
